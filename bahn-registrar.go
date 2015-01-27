b425cf6f5a96ce8305787f92055b0c02b1037a14
Ian White <ian@impressiver.com>
2015-01-26 18:28:00 -0800

    Parse  message payloads

--- bahn-registrar.go
+++ bahn-registrar.go
@@ -4,6 +4,7 @@ import (
   "bufio"
   "flag"
   "fmt"
+  "regexp"
   "io"
   "math/rand"
   "os"
@@ -15,27 +16,50 @@ import (
   MQTT "git.eclipse.org/gitroot/paho/org.eclipse.paho.mqtt.golang.git"
 )
 
-func onSysMessageReceived(mqtt *MQTT.MqttClient, message MQTT.Message) {
-  fmt.Printf("Received message on $SYS topic: %s\n", message.Topic())
-  fmt.Printf("Message: %s\n", message.Payload())
+// MQTT $SYS date form
+const date_form = "0000-00-00 00:00:00-0000"
 
-  // Create a new series w/ data point for the message
-  series := &influx.Series {
-    Name:    message.Topic(),
-    Columns: []string{"value"},
-    Points:  [][]interface{}{
-      {fmt.Sprintf("%s", message.Payload())},
-      },
+// Matcher for the above date form
+var re_date     = regexp.MustCompile(`^[\d]{4}-[\d]{2}-[\d]{2}\s[\d]{2}:[\d]{2}:[\d]{2}-[\d]{4}$`)
+
+// Match numeric values (int, float, etc)
+var re_numeric  = regexp.MustCompile(`^[0-9\.]+$`)
+
+
+//
+// Database
+//
+
+func persist(topic string, p []byte) {
+  // Convert json to string:obj map
+  var data map[string]interface{}
+  if err := json.Unmarshal(p, &data); err != nil {
+    panic(err)
   }
 
-  // Connect to DB
+  // Split map into key/value arrays
+  var keys []string
+  var values []interface{}
+  for key, value := range data {
+    keys = append(keys, key)
+    values = append(values, value)
+  }
+
+  // Create series for the topic, with keys for columns and values for points
+  series := &influx.Series {
+    Name:    topic,
+    Columns: keys,
+    Points:  [][]interface{}{
+      values,
+    },
+  }
+
+  // Create db connection
   c, err := influx.NewClient(&influx.ClientConfig{
     Username: "bahn",
     Password: "b8hn",
     Database: "bahn_registrar",
   })
-
-  // Failed to init DB
   if err != nil {
     panic(err)
   }
@@ -44,6 +68,38 @@ func onSysMessageReceived(mqtt *MQTT.MqttClient, message MQTT.Message) {
   if err := c.WriteSeries([]*influx.Series{series}); err != nil {
     panic(err)
   }
+}
+
+
+//
+// Message handlers
+//
+
+func onSysMessageReceived(mqtt *MQTT.MqttClient, message MQTT.Message) {
+  fmt.Printf("Received message on $SYS topic: %s\n", message.Topic())
+  fmt.Printf("Message: %s ", message.Payload())
+
+  // Unmarshal payload by wrapping in json, converting to bytes, and using the
+  // json unmarshaler to figure out the value type
+  
+  var json_payload []byte
+  if re_numeric.Match(message.Payload()) {
+    fmt.Println("(numeric)")
+    // Let json unmarshaler figure out what type of numeric
+    json_payload = []byte(fmt.Sprintf("{\"value\": %s}", message.Payload()))
+  } else if(re_date.Match(message.Payload())) {
+    fmt.Println("(date)")
+    // Reformat dates as ISO-8601 (w/o nanos)
+    t, _ := time.Parse(date_form, string(message.Payload()[:]))
+    json_payload = []byte(fmt.Sprintf("{\"value\": \"%s\"}", t.Format(time.RFC3339)))
+  } else {
+    fmt.Println("(string)")
+    // Quote the value as a string
+    json_payload = []byte(fmt.Sprintf("{\"value\": \"%s\"}", message.Payload()))
+  }
+
+  // Save the processed message
+  persist(message.Topic(), json_payload)
 }
 
 func onAnyMessageReceived(mqtt *MQTT.MqttClient, message MQTT.Message) {
@@ -55,46 +111,14 @@ func onTopicMessageReceived(mqtt *MQTT.MqttClient, message MQTT.Message) {
   fmt.Printf("OMG!! Received message on MY topic: %s\n", message.Topic())
   fmt.Printf("Message: %s\n", message.Payload())
 
-  // Convert JSON into string:obj map
-  var data map[string]interface{}
-  if err := json.Unmarshal(message.Payload(), &data); err != nil {
-    panic(err)
-  }
-
-  var keys []string
-  var values []interface{}
-  for key, value := range data {
-    keys = append(keys, key)
-    values = append(values, value)
-  }
-
-  // Create a new series w/ data point for the message
-  series := &influx.Series {
-    Name:    message.Topic(),
-    Columns: keys,
-    Points:  [][]interface{}{
-      values,
-    },
-  }
-
-  // Connect to DB
-  c, err := influx.NewClient(&influx.ClientConfig{
-    Username: "bahn",
-    Password: "b8hn",
-    Database: "bahn_registrar",
-  })
-
-  // Failed to init DB
-  if err != nil {
-    panic(err)
-  }
-
-  // Write to db or die trying
-  if err := c.WriteSeries([]*influx.Series{series}); err != nil {
-    panic(err)
-  }
-
+  // Save the processed message
+  persist(message.Topic(), message.Payload())
 }
+
+
+//
+// Main
+//
 
 func main() {
   stdin := bufio.NewReader(os.Stdin)
@@ -105,10 +129,8 @@ func main() {
   user       := flag.String("user", "registrar"+strconv.Itoa(rand.Intn(1000)), "The client id")
   topic      := flag.String("topic", "owntracks/#", "The topic of interest")
   sys        := flag.Bool("sys", true, "Collect $SYS messages")
-  qos        := flag.Int("qos", 0, "The QoS level to use when sending")
-
+  qos        := flag.Int("qos", 0, "The QoS level to use for published messages")
   flag.Parse()
-
 
   // Init MQTT
   opts   := MQTT.NewClientOptions().AddBroker(*broker).SetClientId(*user).SetDefaultPublishHandler(onAnyMessageReceived).SetCleanSession(true)
